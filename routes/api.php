c8ecfcef9195e71b6c51c03d97246ca068c064cd
Lucas Dauto <lucasdauto@gmail.com>
2024-04-05 14:54:50 -0300

    routes api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
+
 <?php
 
+use App\Http\Controllers\API\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::apiResource('users',UsersController::class);
